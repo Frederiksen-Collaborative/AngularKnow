3d028f42c4ecd985c7d61564dd731dba3633596a
sfrederiksen <sfrederiksen@mitre.org>
2016-01-15 09:57:37 -0500

    Added batch setup file

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 cd ..
-npm install -g bower
-bower cache clean
-bower install
+bower install angular/bower-angular#~1.4.8 --save
+bower install angular/bower-angular-resource#~1.4.8 --save
+bower install angular/bower-angular-messages#~1.4.8 --save
+bower install angular-ui/bootstrap-bower#~0.14.3 --save
+bower install gsklee/ngStorage#~0.3.10 --save
+bower install angular/bower-angular-route#~1.4.8 --save
+bower install components/jquery#~2.2.0 --save
+bower install twbs/bootstrap#~3.3.6 --save
 npm install
-echo "Setup complete..."
+echo Setup complete...
